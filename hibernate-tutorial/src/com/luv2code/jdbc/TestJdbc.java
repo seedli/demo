c8cbe0852672efcaff6c3fd42ada9d8675033751
Seed <seedlix@gmail.com>
2016-11-09 01:03:11 +0800

    11/9

--- hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
+++ hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
@@ -1,10 +1,30 @@
 package com.luv2code.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class TestJdbc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-  
+		
+		String jdbcUrl = "jdbc:mysql://192.168.65.130:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn =
+					DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection successful!!!");
+			
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		
+		
 	}
 
 }
